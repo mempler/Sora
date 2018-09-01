6361d3759e043c85036f668c929e5cabfa1ae3f4
Mempler <me@mempler.de>
2018-09-01 11:14:21 +0200

    Add project files.

---
+++ Kaoiji/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaoiji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
