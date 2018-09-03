c59f81fb5abe8bafc4a7d65fde29ce951a4d96f1
Mempler <me@mempler.de>
2018-09-03 14:36:19 +0200

    Maybe a better way to write packets ?

--- Kaoiji/packets/packet.cs
+++ Kaoiji/packets/packet.cs
@@ -1,31 +1,45 @@
 ﻿using System.IO;
 
 using Kaoiji.enums;
+using Kaoiji.helpers;
 
 namespace Kaoiji.packets
 {
+    public class PacketWriter
+    {
+        private BinaryWriter bw;
+        public void Announce(string msg)
+        {
+            using (MemoryStream s = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(s))
+            {
+                Packet p = new Packet(PacketIDs.Server_Announce, s);
+                Binary.WriteOsuString(msg, bw);
+                Packet.WritePacket(p, bw);
+            }
+        }
+    }
     public class Packet
     {
-        private Stream PacketData;
-        public PacketIDs PacketID;
-
-        public void Write(BinaryWriter w) => WritePacket(this, w);
+        public Stream Data { get; }
+        public PacketIDs ID;
+        public Packet(PacketIDs ID, Stream Data) { this.ID = ID; this.Data = Data; }
+        public Packet() { }
 
         public static void WritePacket(Packet p, BinaryWriter w)
         {
-            w.Write((short)p.PacketID);
+            w.Write((short)p.ID);
             w.Write((byte)0);
-            w.Write(p.PacketData == null ? 0 : p.PacketData.Length);
-            p.PacketData.CopyTo(w.BaseStream);
+            w.Write(p.Data == null ? 0 : p.Data.Length);
+            p.Data.CopyTo(w.BaseStream);
             w.Close();
         }
-
         public static Packet ReadPacket(BinaryReader r)
         {
             Packet p = new Packet();
-            p.PacketID = (PacketIDs)r.ReadInt16();
+            p.ID = (PacketIDs)r.ReadInt16();
             r.BaseStream.Position++;
-            r.BaseStream.CopyTo(p.PacketData, r.ReadInt32());
+            r.BaseStream.CopyTo(p.Data, r.ReadInt32());
             return p;
         }
     }
