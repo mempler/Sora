2707111b9891c83faf7f6232ea332bf97e4039e8
Mempler <me@mempler.de>
2018-09-05 14:22:05 +0200

    add ToString

--- Kaoiji/consts/LoginData.cs
+++ Kaoiji/consts/LoginData.cs
@@ -8,11 +8,16 @@
         public int TimeOff;
         public SecurityHash SecurityHash;
         public bool BlockNonFriendsDM;
+
+        public override string ToString() =>
+             $"Username: {Username} Password: {Password} Osu Version: {OsuVersion} TimeOffset: {TimeOff * 12} SecurityHash: {SecurityHash} Block Nonfriend DMs {BlockNonFriendsDM}";
     }
     public class SecurityHash
     {
         public string OsuHash;
         public string DiskMD5;
         public string UniqueMD5;
+        public override string ToString() =>
+            $"OsuHash: {OsuHash} DiskMD5: {DiskMD5} UniqueMD5: {UniqueMD5}";
     }
 }
