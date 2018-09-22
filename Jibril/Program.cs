bd80d082223d378986fc60302af885a83675d64d
Mempler <me@mempler.de>
2018-09-22 22:37:54 +0200

    Adding Jibril

---
+++ Jibril/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Jibril
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: PUT CODE HERE.
+            // This is gonna be the /web/ server aka Private API
+        }
+    }
+}
