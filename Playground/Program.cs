d87e59523a40d571fee4a172bee3757860e0db59
Mempler <me@mempler.de>
2018-09-23 00:37:09 +0200

    Playground

---
+++ Playground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/* Just a playground for me to play around. */
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
