db14bae203a3ac329a99f75f693ad187ab5ceb6c
Mempler <me@mempler.de>
2018-10-11 07:03:11 +0200

    Hex Encoding.

---
+++ Shared/Helpers/Hex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shared.Helpers
+{
+    public static class Hex
+    {
+        // https://stackoverflow.com/questions/724862/converting-from-hex-to-string
+        public static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            var raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+    }
+}
