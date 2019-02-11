f8ca6474612537bb18cd1b2ced983491a17d4ea4
Mempler <me@mempler.de>
2019-02-11 08:00:22 +0100

    Move Handlers

--- EventManager/Enums/EventType.cs
+++ EventManager/Enums/EventType.cs
@@ -2,6 +2,71 @@ namespace EventManager.Enums
 {
     public enum EventType
     {
-        
+        Initializer,
+
+        #region Bancho
+
+        BanchoPacketHandler,
+        BanchoLoginHandler,
+        BanchoSendUserStatus,
+        BanchoSendIrcMessage,
+        BanchoExit,
+        BanchoRequestStatusUpdate,
+        BanchoPong,
+        BanchoStartSpectating,
+        BanchoStopSpectating,
+        BanchoSpectateFrames,
+        BanchoErrorReport,
+        BanchoCantSpectate,
+        BanchoSendIrcMessagePrivate,
+        BanchoLobbyPart,
+        BanchoLobbyJoin,
+        BanchoMatchCreate,
+        BanchoMatchJoin,
+        BanchoMatchPart,
+        BanchoMatchChangeSlot,
+        BanchoMatchReady,
+        BanchoMatchLock,
+        BanchoMatchChangeSettings,
+        BanchoMatchStart,
+        BanchoMatchScoreUpdate,
+        BanchoMatchComplete,
+        BanchoMatchChangeMods,
+        BanchoMatchLoadComplete,
+        BanchoMatchNoBeatmap,
+        BanchoMatchNotReady,
+        BanchoMatchFailed,
+        BanchoMatchHasBeatmap,
+        BanchoMatchSkipRequest,
+        BanchoChannelJoin,
+        BanchoBeatmapInfoRequest,
+        BanchoMatchTransferHost,
+        BanchoFriendAdd,
+        BanchoFriendRemove,
+        BanchoMatchChangeTeam,
+        BanchoChannelLeave,
+        BanchoReceiveUpdates,
+        BanchoSetIrcAwayMessage,
+        BanchoUserStatsRequest,
+        BanchoInvite,
+        BanchoMatchChangePassword,
+        BanchoSpecialMatchInfoRequest,
+        BanchoUserPresenceRequest,
+        BanchoUserPresenceRequestAll,
+        BanchoUserToggleBlockNonFriendPm,
+        BanchoMatchAbort,
+        BanchoSpecialJoinMatchChannel,
+        BanchoSpecialLeaveMatchChannel,
+
+        #endregion
+
+        #region PrivateAPI
+
+        SharedAvatars,
+        SharedScoreboardRequest,
+        SharedScoreSubmittion,
+        SharedGetReplay,
+
+        #endregion
     }
 }
