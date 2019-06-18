3fcfb093fb21ef4e7b7b2e29e7ddd409f29c263b
Mempler <me@mempler.de>
2019-06-18 13:30:51 +0200

    Still WIP

--- Ripple.MergeTool/Program.cs
+++ Ripple.MergeTool/Program.cs
@@ -22,56 +22,69 @@ namespace Ripple.MergeTool
             SoraDbContextFactory factory = new SoraDbContextFactory();
             RippleDbContext rippleCtx = new RippleDbContext();
             
-            using DatabaseWriteUsage db = factory.GetForWrite();
-            
             #region User Database merge
-            foreach (RippleUser user in rippleCtx.Users)
-            {
-                if (user.id == 999) // We don't need fokabot! gtfu
-                    continue;
 
-                if (user.email == null || user.password_md5 == null || user.username == null)
+            using (DatabaseWriteUsage db = factory.GetForWrite()) {
+                foreach (RippleUser user in rippleCtx.Users)
                 {
-                    Logger.Info($"Invalid User! {user.id}");
-                    Thread.Sleep(1000);
-                    continue;
+                    if (db.Context.Users.Any(x => x.Username == user.username)) // Skip double Users
+                        continue;
+                    
+                    if (user.id == 999) // We don't need fokabot! gtfu
+                        continue;
+
+                    if (user.email == null || user.password_md5 == null || user.username == null)
+                    {
+                        Logger.Info($"Invalid User! {user.id}");
+                        Thread.Sleep(1000);
+                        continue;
+                    }
+
+                    Logger.Info($"User: {user.username} ( {user.id} )");
+
+                    db.Context.Users.Add(new Users
+                    {
+                        Username   = user.username,
+                        Password   = user.password_md5,
+                        Privileges = PrivilegeMerger.Merge(user.privileges),
+                        Email      = user.email
+                    });
                 }
-
-                Logger.Info($"User: {user.username} ( {user.id} )");
-
-                db.Context.Users.Add(new Users
-                {
-                    Username   = user.username,
-                    Password   = user.password_md5,
-                    Privileges = PrivilegeMerger.Merge(user.privileges),
-                    Email      = user.email
-                });
             }
             #endregion
             
             #region Beatmap Databse merge
-            foreach (RippleBeatmap rMap in rippleCtx.Beatmaps)
-            {
-                Logger.Info($"Importing {rMap.beatmap_id}");
-                
-                Beatmaps sMap;
-                if ((sMap = Beatmaps.FetchFromApi(cfg, null, rMap.beatmap_id)) != null)
-                {
-                    sMap.RankedStatus = rMap.ranked;
-                    Logger.Info(cfg.CRipple.LetsBeatmapPath + "/" + sMap.Id + ".osu");
-                    Logger.Info(cfg.SoraDataDirectory + "/beatmaps/" + sMap.FileMd5);
 
-                    if (File.Exists(cfg.CRipple.LetsBeatmapPath + "/" + sMap.Id + ".osu") && !File.Exists(
-                            cfg.SoraDataDirectory + "/beatmaps/" + sMap.FileMd5))
-                        File.Copy(cfg.CRipple.LetsBeatmapPath + "/" + sMap.Id + ".osu",
-                                  cfg.SoraDataDirectory + "/beatmaps/" + sMap.FileMd5);
-                    else
-                        BeatmapDownloader.GetBeatmap(sMap.FileMd5, cfg);
+            using (DatabaseWriteUsage db = factory.GetForWrite())
+            {
+                foreach (RippleBeatmap rMap in rippleCtx.Beatmaps)
+                {
+                    if (db.Context.Beatmaps.Any(x => x.Id == rMap.beatmap_id))
+                        continue;
                     
-                    if (db.Context.Beatmaps.Count(x => x.Id == sMap.Id) < 1)
-                        db.Context.Beatmaps.Add(sMap);
-                    else
-                        db.Context.Beatmaps.Update(sMap);
+                    Logger.Info($"Importing {rMap.beatmap_id}");
+
+                    Beatmaps sMap;
+                    if ((sMap = Beatmaps.FetchFromApi(cfg, null, rMap.beatmap_id)) != null)
+                    {
+                        sMap.RankedStatus = rMap.ranked;
+
+                        string LetsMapPath = Path.Join(cfg.CRipple.LetsBeatmapPath, "/" + sMap.Id + ".osu");
+                        string SoraMapPath = Path.Join(cfg.SoraDataDirectory, "/beatmaps/" + sMap.FileMd5);
+
+                        Logger.Info(LetsMapPath);
+                        Logger.Info(SoraMapPath);
+                        
+                        if (File.Exists(LetsMapPath) && !File.Exists(SoraMapPath))
+                            File.Copy(LetsMapPath, SoraMapPath);
+                        else
+                            BeatmapDownloader.GetBeatmap(sMap.FileMd5, cfg);
+
+                        if (db.Context.Beatmaps.Count(x => x.Id == sMap.Id) < 1)
+                            db.Context.Beatmaps.Add(sMap);
+                        else
+                            db.Context.Beatmaps.Update(sMap);
+                    }
                 }
             }
             #endregion
